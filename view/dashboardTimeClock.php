10f724b7f451373e5d6e826ec2a1c3a887086ccc
Eli Smithson <elijah.smithson@gmail.com>
2019-03-27 12:59:21 -0500

    Added stubs for timeclock

---
+++ view/dashboardTimeClock.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Dashboard Main</title>
+	<?php include("../CSS/includeBack.php");?>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-inverse visible-xs">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+                <a class="navbar-brand" href="#">Logo</a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li><a href="dashboard.php">Dashboard</a></li>
+                    <li><a href="dashboard_quote.php">Quote List</a></li>
+
+                    <li><a href="followUp.php">Follow Up</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container-fluid" id="Dash">
+        <div class="row content">
+            <div class="col-sm-3 sidenav hidden-xs" id="myNavbar">
+                <h2><img src="../images/ABTImages/abtwater.png" alt="ABT" class="img-responsive" class="avatar"></h2>
+                <ul class="nav nav-pills nav-stacked">
+                    <li><a href="dashboard.php">Dashboard</a></li>
+                    <li><a href="dashboard_quote.php">Quotes List</a></li>
+
+                    <li><a href="followUp.php">Follow Up</a></li>
+
+
+
+
+                </ul><br>
+            </div>
+            <br>
+			<?php
+				
+			?>
+
+            <!--Start Code Here-->
+
+            
+                </body>
+
+</html>
