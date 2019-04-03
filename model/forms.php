b803708686abd587d4cb06d4b219eb12492a3d41
pollarde <48774442+pollarde@users.noreply.github.com>
2019-04-03 16:57:15 -0500

    Update forms.php

--- model/forms.php
+++ model/forms.php
@@ -14,28 +14,35 @@ class signUpForm
         <form role="form" method="post" action="registration.php?add=<?php echo uniqid()?>">
         <div class="signUp">
             <h1>Sign Up</h1>
+            <p><span class="error" style="color:red">* required field</span></p>
             <br>
             <div class="signUpBody">
+                <span class = "error" style="color: red">*</span>
                 <label for="employeeID"><b>Last 4 of Social</b></label>
-                <input type="text" name="employeeID" required>
+                <input type="number"  minlength="4" maxlength="4" title="Must be 4 numbers" name="employeeID" required>
+                <br/>
                 <br/>
 
+                <span class = "error" style="color: red">*</span>
                 <label for="firstName"><b>Name</b></label>
-                <input type="text" name="firstName" required>
+                <input type="text" title="First and Last Name" name="firstName" required>
+                <br/>
                 <br/>
 
-                
-
-                
-
+                <span class = "error" style="color: red">*</span>
                 <label for="username"><b>Username</b></label>
                 <input type="text" name="username" required>
                 <br/>
-
-                <label for="password"><b>Password</b></label>
-                <input type="password" name="bpassword" required>
                 <br/>
 
+                <span class = "error" style="color: red">*</span>
+                <label for="password"><b>Password</b></label>
+                <input type="password" name="bpassword"  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                       title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"required>
+                <br/>
+                <br/>
+
+                <span class = "error" style="color: red">*</span>
                 <label for="confirmPassword"><b>Confirm Password</b></label>
                 <input type="password" name="confirmPassword" required>
                 <br/>
@@ -63,16 +70,22 @@ class signUpForm
     <form action = 'abtLogin.php' method="POST">
         <div class="login">
             <h1>Login</h1>
+            <p><span class="error" style="color:red">* required field</span></p>
             <br>
             <div class="loginBody">
+                <span class = "error" style="color: red">*</span>
                 <label for="employeeID"><b>Last 4 of Social</b></label>
-                <input type="text" name="employeeID" required>
+                <input type="number"  minlength="4" maxlength="4" title="Must be 4 numbers" name="employeeID" required>
+                <br/>
                 <br/>
 
+                <span class = "error" style="color: red">*</span>
                 <label for="username"><b>Username</b></label>
                 <input type="text" name="username" required>
                 <br/>
+                <br/>
 
+                <span class = "error" style="color: red">*</span>
                 <label for="password"><b>Password</b></label>
                 <input type="password" name="bpassword" required>
                 <br/>
