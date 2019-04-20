d7f1290de8773a8cccc38a15c3efe3d3d050d619
rjordan2 <rjordan2@students.blackhawk.edu>
2019-04-20 10:47:05 -0500

    put clock in timeclock page

--- model/TimeClockFunctions.php
+++ model/TimeClockFunctions.php
@@ -2,43 +2,45 @@
 class timeClockForm{
 	public function displayTimeClock(){
 		?>
-		<form action = 'ClockEmployee.php' method="POST" ID = "clockForm">
-            <div class="Clock container">
+		<form class="form-control" action = 'ClockEmployee.php' method="POST">
+            <div class="Clock">
+            
                 <h2>Clock In / Out</h2>
-                <label for="employeeID" class = "ClockLabel"><b>Employee ID</b></label>
-                <input type="text" name="employeeID" required class = "ClockInput">
-          
-				
+                <br>
 
-				<label for="password" class = "ClockLabel"><b>Password</b></label>
-				<input type="text" name="password" required class = "ClockInput">
-			
-			
-				
-				<br/>
+                <div class="col-md-9 text-center" class="form-group row">
+                <label for="employeeID"><b>Employee ID</b></label>
+                <input class="form-control text-center" type="text" name="employeeID" required>
+                <br>
+                </div>
 
-                <label for="clockType" class = "ClockLabel"><b>Clock Type</b></label>
-                <input type="text" name="clockType" value = "Driving / Installing" class = "ClockInput">
-				<br/>
-				<div class = "ClockLabel">
+                <div class="col-md-9 text-center" class="form-group row">
+                <label for="password"><b>Password</b></label>
+                <input class="form-control text-center " type="text" name="password" required>
+                <br>
+                </div>
+
+				<div class="col-md-9 text-center" class="form-group row">
 					<input type = "radio" name = "clockType" value= "in" id="clockInRadio" required>
-					<label for="clockInRadio" >Clock In</label>
+					<label class="control-label" for="clockInRadio" >Clock In</label>
 					
 					<input type = "radio" name = "clockType" value= "out" id="clockOutRadio" required>
-					<label for="clockOutRadio" >Clock In</label>
+                    <label class="control-label" for="clockOutRadio" >Clock In</label>
+                    <br><br>
+                </div>
+
+                <div class="col-md-9 text-center" class="form-group row">
+                    <button for="submit">Submit</button>
 				</div>
-                <button for="submit" class = "btn primary ClockBtn">Submit</button>
+				
+
+                
             </div>
         </form>
 		
 		
 		<?php		
 	}
-	public function clocktime(){
-		
-	}
-	
-	
 	
 }
 
--- view/dashboardTimeClock.php
+++ view/dashboardTimeClock.php
@@ -6,8 +6,10 @@ include("../model/TimeClockFunctions.php");
 <html lang="en">
 
 <head>
-    <title>Dashboard Main</title>
-	<?php include("../CSS/includeBack.php");?>
+    <title>Time Clock</title>
+	<?php include("../CSS/includeBack2.php");?>
+
+    
 </head>
 
 <body>
@@ -20,14 +22,14 @@ include("../model/TimeClockFunctions.php");
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-                <a class="navbar-brand" href="#">Logo</a>
+      <a class="navbar-brand" href="www.abtwater.com">ABT</a>
             </div>
             <div class="collapse navbar-collapse" id="myNavbar">
                 <ul class="nav navbar-nav">
                     <li><a href="dashboard.php">Dashboard</a></li>
                     <li><a href="dashboard_quote.php">Quote List</a></li>
-
-                    <li><a href="followUp.php">Follow Up</a></li>
+                    <li><a href="contactUs.php">Contact Us</a></li>
+                    <li><a href="dashboardTimeClock.php">Timeclock</a></li>
                 </ul>
             </div>
         </div>
@@ -40,23 +42,119 @@ include("../model/TimeClockFunctions.php");
                 <ul class="nav nav-pills nav-stacked">
                     <li><a href="dashboard.php">Dashboard</a></li>
                     <li><a href="dashboard_quote.php">Quotes List</a></li>
-
-                    <li><a href="followUp.php">Follow Up</a></li>
-
-
-
-
+                    <li><a href="contactUs.php">Contact Us</a></li>
+                    <li><a href="dashboardTimeClock.php">Timeclock</a></li>
                 </ul><br>
             </div>
             <br>
+
+ <!--Start Code Here-->
+
+
+            
+
+ <div class="col-md-9 text-center" class="container-fluid bg-grey" id="clock">
+            <canvas id="canvas" width="400" height="400"
+                    style="background-color:#333">
+            </canvas>
+            
+           </div>
+            
+
+
+            <div class="container-fluid text-center" id="timeClock">
 			<?php
-				session_start();
+			//session_start(); gave error so commented out...seems ok without it
 				$form = new timeClockForm();
 				$form ->displayTimeClock();
 			?>
+            </div>
+            
+           
 
-            <!--Start Code Here-->
 
+<script>
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var radius = canvas.height / 2;
+ctx.translate(radius, radius);
+radius = radius * 0.90
+setInterval(drawClock, 1000);
+
+function drawClock() {
+  drawFace(ctx, radius);
+  drawNumbers(ctx, radius);
+  drawTime(ctx, radius);
+}
+
+function drawFace(ctx, radius) {
+  var grad;
+  ctx.beginPath();
+  ctx.arc(0, 0, radius, 0, 2*Math.PI);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+  grad = ctx.createRadialGradient(0,0,radius*0.95, 0,0,radius*1.05);
+  grad.addColorStop(0, '#333');
+  grad.addColorStop(0.5, 'white');
+  grad.addColorStop(1, '#333');
+  ctx.strokeStyle = grad;
+  ctx.lineWidth = radius*0.1;
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(0, 0, radius*0.1, 0, 2*Math.PI);
+  ctx.fillStyle = '#333';
+  ctx.fill();
+}
+
+function drawNumbers(ctx, radius) {
+  var ang;
+  var num;
+  ctx.font = radius*0.15 + "px arial";
+  ctx.textBaseline="middle";
+  ctx.textAlign="center";
+  for(num = 1; num < 13; num++){
+    ang = num * Math.PI / 6;
+    ctx.rotate(ang);
+    ctx.translate(0, -radius*0.85);
+    ctx.rotate(-ang);
+    ctx.fillText(num.toString(), 0, 0);
+    ctx.rotate(ang);
+    ctx.translate(0, radius*0.85);
+    ctx.rotate(-ang);
+  }
+}
+
+function drawTime(ctx, radius){
+    var now = new Date();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+    //hour
+    hour=hour%12;
+    hour=(hour*Math.PI/6)+
+    (minute*Math.PI/(6*60))+
+    (second*Math.PI/(360*60));
+    drawHand(ctx, hour, radius*0.5, radius*0.07);
+    //minute
+    minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
+    drawHand(ctx, minute, radius*0.8, radius*0.07);
+    // second
+    second=(second*Math.PI/30);
+    drawHand(ctx, second, radius*0.9, radius*0.02);
+}
+
+function drawHand(ctx, pos, length, width) {
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.lineCap = "round";
+    ctx.moveTo(0,0);
+    ctx.rotate(pos);
+    ctx.lineTo(0, -length);
+    ctx.stroke();
+    ctx.rotate(-pos);
+}
+</script>
+            
             
                 </body>
 
