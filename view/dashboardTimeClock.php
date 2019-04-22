2fdbebcfcbc7cef4f5f3c01377169de47512b79e
rjordan2 <rjordan2@students.blackhawk.edu>
2019-04-22 14:37:45 -0500

    placed clock

--- view/dashboardTimeClock.php
+++ view/dashboardTimeClock.php
@@ -9,7 +9,10 @@ include("../model/TimeClockFunctions.php");
     <title>Time Clock</title>
 	<?php include("../CSS/includeBack2.php");?>
     <link href="css/style.css" rel="stylesheet" />
-    
+    <!-- JavaScript Includes -->
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.0.0/moment.min.js"></script>
+		<script src="js/script.js"></script>
 </head>
 
 <body>
@@ -39,50 +42,58 @@ include("../model/TimeClockFunctions.php");
         <div class="row content">
             <div class="col-sm-3 sidenav hidden-xs" id="myNavbar">
                 <h2><img src="../images/ABTImages/abtwater.png" alt="ABT" class="img-responsive" class="avatar"></h2>
+                
+            
                 <ul class="nav nav-pills nav-stacked">
+                <div  id="clock"  class="light">
+			<div class="container-fluid" class="display">
+				<div class="weekdays"></div>
+				<div class="ampm"></div>
+				
+				<div class="digits"></div>
+			</div>
+		</div>
                     <li><a href="dashboard.php">Dashboard</a></li>
                     <li><a href="dashboard_quote.php">Quotes List</a></li>
                     <li><a href="contactUs.php">Contact Us</a></li>
                     <li><a href="dashboardTimeClock.php">Timeclock</a></li>
+
                 </ul><br>
-            </div>
-            <br>
+                </div>
+                </div>
+                </div>
+                </div>
+            
 
  <!--Start Code Here-->
 
 
-            
+ 
+ 
 
- <div class="col-sm-12 text-center" class="container-fluid" id="clock">
- <div id="clock" class="light">
-			<div class="display">
-				<div class="weekdays"></div>
-				<div class="ampm"></div>
-				<div class="alarm"></div>
-				<div class="digits"></div>
-			</div>
-		</div>
+ 
 
-</div>
+
 
 		
         
-		<!-- JavaScript Includes -->
-		<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-		<script src="http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.0.0/moment.min.js"></script>
-		<script src="js/script.js"></script>
+		
             
            
             
 
-
-            <div class="container-fluid text-center" id="timeClock">
+ 
+                   
+                <div class="row">
+                    <div class="col-sm-4">>
 			<?php
 			//session_start(); gave error so commented out...seems ok without it
 				$form = new timeClockForm();
 				$form ->displayTimeClock();
 			?>
             </div>
+            </div>
+            
             
            
 
